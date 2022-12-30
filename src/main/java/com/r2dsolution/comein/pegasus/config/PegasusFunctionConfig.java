2d8a1dcf546be48ca48a0cf333dbe67bc4738780
Tawatchai Radom <tawatchai@r2dsolution.com>
2022-12-31 02:58:25 +0700

    initial all config

--- src/main/java/com/r2dsolution/comein/pegasus/config/PegasusFunctionConfig.java
+++ src/main/java/com/r2dsolution/comein/pegasus/config/PegasusFunctionConfig.java
@@ -1,8 +1,10 @@
 package com.r2dsolution.comein.pegasus.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.r2dsolution.comein.pegasus.event")
 public class PegasusFunctionConfig {
 
 }
